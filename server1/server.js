7e5717dcfe6f1512d5fafea5d6317f77741a8419
tanchao <1509382637@qq.com>
2017-11-23 09:17:13 +0800

    first commit

---
+++ server1/server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var app = express();
+var url = require('url'); 
+ app.use(express.static('web/public'));
+//  主页输出 "Hello World"
+app.get('/', function (req, res) {
+   console.log("主页 GET 请求");
+   res.send('Hello GET');
+})
+app.get('/index', function (req, res) {
+   res.sendFile( __dirname + "/" + "web/index.html" );
+})
+ 
+ 
+//  POST 请求
+app.post('/', function (req, res) {
+   console.log("主页 POST 请求");
+   res.send('Hello POST');
+})
+app.get('/show', function (req, res) {
+  var queryObj = url.parse(req.url,true).query;  
+    res.send(queryObj.callback+'(\'{"message": "展示ID为' +queryObj.fileId+'的信息"}\')');
+})
+app.get('/process_get', function (req, res) {
+ 
+   // 输出 JSON 格式
+   var response = {
+       "first_name":req.query.first_name,
+       "last_name":req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+ 
+//  /del_user 页面响应
+app.get('/del_user', function (req, res) {
+   console.log("/del_user 响应 DELETE 请求");
+   res.send('删除页面');
+})
+ 
+//  /list_user 页面 GET 请求
+app.get('/list_user', function (req, res) {
+   console.log("/list_user GET 请求");
+   res.send('用户列表页面');
+})
+ 
+// 对页面 abcd, abxcd, ab123cd, 等响应 GET 请求
+app.get('/ab*cd', function(req, res) {   
+   console.log("/ab*cd GET 请求");
+   res.send('正则匹配');
+})
+ 
+ 
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
